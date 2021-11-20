bcedb7dae31b8f914141da86ebd89a1841f5bc8e
A DIONN <ashley.muldrow@yahoo.com>
2021-11-20 18:50:10 -0500

    passwordlength

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Assignment code here
-var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
 
@@ -9,6 +8,19 @@ var number = ["0123456789"]
 var characters = ["!@#$%&*"]
 var selectedArray = [];
 
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 
 // Alert with password criteria 
 window.alert("The criteria for a password is lowercase , uppercase , number and character");
@@ -16,26 +28,25 @@ window.alert("The criteria for a password is lowercase , uppercase , number and 
 var validInput =true;
 
 // Asks for the length of password 
-var promptLength = function () {
-  var promptLength = window.prompt("How long would you like for your password to be ? It must be betweeen 8-28 character")
+function generatePassword() {
+  var passwordlength = parseInt(prompt("How long would you like for your password to be ? It must be betweeen 8-28 character")
  
   // validate  answer 
-  if (promptLength === "" || promptLength === null) {
-    window.alert("You need to provide a valid answer! Please try again")
+  if (passwordlength < 8 || passwordLength > 128) {
+    window.alert("Password must be between 8 and 128 characters")
     // return will call it again and stop the rest of the function 
-    return promptLength();
+    return generatePassword();
+ }
+ // if no number is inserted this alert will show
+ if (isNaN(passwordlength)) {
+   window.alert("You must enter a valid number")
+   return null;
  }
 
+
+   
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
 
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
