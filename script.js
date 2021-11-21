e0ee5ff302ca979a96ec6b142502c84b762b03db
A DIONN <ashley.muldrow@yahoo.com>
2021-11-20 20:03:19 -0500

    lengthpt2

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
 // Assignment code here
+var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
+// Arrays for lowercase , uppercase , numbers and characters
 
 var lowercase = ["abcdefghijklmnopqrstuvwxyz"]
 var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
 var number = ["0123456789"]
-var characters = ["!@#$%&*"]
+var specialcharacters = ["!@#$%&*"]
 var selectedArray = [];
 
+var password;
+
+
+// Asks for the length of password 
+function generatePassword() {
+  // Length of the password is chosen 
+  var password = parseInt(window.prompt("How long would you like for your password to be ? It must be betweeen 8-28 character.");
+    if(password < 8 || password > 128) {
+    window.alert("Password must be between 8 and 128 characters.");
+    return null;
+  }
+    else if(isNaN(password)) {
+    window.alert("You must enter a valid number");
+    return null;
+  }
+
+  //character types to include in password
+
+
+
+
+
+
+
+ // if no number is inserted this alert will show
+  // if (isNaN(passwordl)) { window.alert("You must enter a valid number"); return null;
+ } 
+
+
+ 
+
+
+   
+// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -20,33 +55,4 @@ function writePassword() {
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-// Alert with password criteria 
-window.alert("The criteria for a password is lowercase , uppercase , number and character");
-
-var validInput =true;
-
-// Asks for the length of password 
-function generatePassword() {
-  var passwordlength = parseInt(prompt("How long would you like for your password to be ? It must be betweeen 8-28 character")
- 
-  // validate  answer 
-  if (passwordlength < 8 || passwordLength > 128) {
-    window.alert("Password must be between 8 and 128 characters")
-    // return will call it again and stop the rest of the function 
-    return generatePassword();
- }
- // if no number is inserted this alert will show
- if (isNaN(passwordlength)) {
-   window.alert("You must enter a valid number")
-   return null;
- }
-
-
-   
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-
 
